460576211fe1f60018572e7b5537917acba0e9f6
Nikhil <devsotnik@gmail.com>
2022-08-31 19:59:36 +0530

    calc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [data,setData] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="App">
+            <div className="input-type" >
+              <input type="text" value={data} placeholder="0" />
+            </div>
+
+            <div className="button-type">
+                <button id="clear" onClick={()=>{setData("")}}>Clear</button>
+                <button onClick={()=>{setData(data.slice(0,-1))}}>Del</button>
+                <button onClick={()=>{setData(data.concat("/"))}}>/</button>
+                <button onClick={()=>{setData(data.concat("7"))}}>7</button>
+                <button onClick={()=>{setData(data.concat("8"))}}>8</button>
+                <button onClick={()=>{setData(data.concat("9"))}}>9</button>
+                <button onClick={()=>{setData(data.concat("*"))}}>*</button>
+                <button onClick={()=>{setData(data.concat("4"))}}>4</button>
+                <button onClick={()=>{setData(data.concat("5"))}}>5</button>
+                <button onClick={()=>{setData(data.concat("6"))}}>6</button>
+                <button onClick={()=>{setData(data.concat("-"))}}>-</button>
+                <button onClick={()=>{setData(data.concat("1"))}}>1</button>
+                <button onClick={()=>{setData(data.concat("2"))}}>2</button>
+                <button onClick={()=>{setData(data.concat("3"))}}>3</button>
+                <button onClick={()=>{setData(data.concat("+"))}}>+</button>
+                <button onClick={()=>{setData(data.concat("0"))}}>0</button>
+                <button onClick={()=>{setData(data.concat("."))}}>.</button>
+                <button id="equal" onClick={()=>{setData(eval(data).toString())}}>=</button>
+            </div>
+          </div>
   );
 }
 
